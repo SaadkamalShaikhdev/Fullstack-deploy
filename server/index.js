9888e7e9589cc151eda339bb0d5a616fb4dea770
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2025-11-15 00:36:49 +0500

    Third change in url to solve problem in production

--- server/index.js
+++ server/index.js
@@ -4,20 +4,16 @@ const app = express()
 const port = 4000
 
 app.use(express.json())
-app.use(cors(
-   {
-     origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:3000",
-        "http://72.61.114.23:4000",
-        "http://72.61.114.23:4000/api/message"
-        // Add Production Url to get data if You dont add it it doesnot work
-    ],
-    credentials: true,
-
-   }
-))
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "http://72.61.114.23:5173",   // production frontend
+    "http://72.61.114.23:4000"    // NOT /api/message
+  ],
+  credentials: true,
+}));
 app.get('/api/message', (req, res) => {
   res.json({message: "Hello from Saadkamal Server"})
 })
